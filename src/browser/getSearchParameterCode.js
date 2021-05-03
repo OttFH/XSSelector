3b7b6641d83f5110aab3e26b163b65459b571ccf
Clemens Ott <42971547+ShelbyBoss@users.noreply.github.com>
2021-05-03 18:28:11 +0200

    improved search for parameters

--- src/browser/getSearchParameterCode.js
+++ src/browser/getSearchParameterCode.js
@@ -3,6 +3,37 @@ const {callFunctionCode} = require('../utils');
 const searchFunction = () => {
     const inputElementSelector = 'input, textarea, select';
 
+    function getDefaultValue(type) {
+        switch (type) {
+            case 'checkbox':
+            case 'radio':
+                return 1;
+            case 'color':
+                return '#000000';
+            case 'date':
+                return new Date().toISOString().split('T')[0];
+            case 'datetime-local':
+                return new Date().toISOString();
+            case 'email':
+                return 'examle@mail.com';
+            case 'month':
+                return new Date().toISOString().substr(0, 7);
+            case 'number':
+            case 'range':
+            case 'select-one':
+                return '234';
+            case 'time':
+                return new Date().toISOString().split('T')[1];
+            case 'url':
+                return 'www.example.com';
+            case 'week':
+                const now = new Date();
+                const approximateWeek = Math.round((now.getMonth() * 30 + now.getDate()) / 7);
+                return `${now.getFullYear()}-W${approximateWeek}`;
+        }
+        return null;
+    }
+
     const postFormInputs = [...document.querySelectorAll('form')]
         .filter(form => form.method && form.method.toLowerCase() === 'post')
         .map(form => [...form.querySelectorAll(inputElementSelector)]).flat()
@@ -11,18 +42,24 @@ const searchFunction = () => {
     const otherInputs = [...document.querySelectorAll(inputElementSelector)]
         .filter(e => !postFormInputs.includes(e));
 
-
     function getDistinct(inputs) {
-        return Object.entries(inputs.reduce((all, input) => {
-            all[input.name] = all.value || input.value || '';
+        return Object.values(inputs.reduce((all, input) => {
+            all[input.name] = {
+                key: input.name,
+                value: all[input.name] && all[input.name].value || input.value || input.checked || '',
+                type: input.type,
+            };
             return all;
-        }, {})).map(([key, value]) => ({key, value}));
+        }, {})).map(({key, value, type}) => ({
+            key,
+            value: value || getDefaultValue(type),
+        }));
     }
 
     return {
         body: getDistinct(postFormInputs),
         query: getDistinct(otherInputs),
-    }
+    };
 }
 
 function getSearchParameterCode() {
